f90f35680a25035e3eec0dbd1eb5a7e9aef51572
zimr978-ui <zimr978@gmail.com>
2026-05-13 12:59:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!' )
 st.text_input('이름을 입력하세요')
-st.selectbox('좋아하는 음식을 선택하세요',['떡볶이','치킨','마라탕','두쫀쿠']
+st.selectbox('좋아하는 음식을 선택하세요',['떡볶이','치킨','마라탕','두쫀쿠'])
 st.button('인사말 생성')

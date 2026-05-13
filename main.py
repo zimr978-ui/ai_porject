2b30ee6858a96aaf3d9aea2d1d9e3dba8eb1a73e
zimr978-ui <zimr978@gmail.com>
2026-05-13 13:10:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ st.title('나의 첫 웹 서비스 만들기!' )
 a=st.text_input('이름을 입력하세요')
 b=st.selectbox('좋아하는 음식을 선택하세요',['떡볶이','치킨','마라탕','두쫀쿠'])
 if st.button('인사말 생성'):
-  st.write(a+'님! 안녕하세요!')
+   st.write(a+'님! 안녕하세요!')
+   st.info('반갑습니다')
+   st.warning(b+'음식을 좋아하시는군요!')
+   st.error('잘 지내봐요!')
